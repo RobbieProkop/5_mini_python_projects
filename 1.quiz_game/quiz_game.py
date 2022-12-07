6686c49dd8531c716ba34953e1cc9217af01650d
Robbie Prokop <robbieprokop@gmail.com>
2022-12-07 15:07:06 +0400

    begin quiz game

---
+++ 1.quiz_game/quiz_game.py
@@ -0,0 +1,11 @@
+print('Welcome to the quiz!')
+
+playing = input("Would you like to play? ")
+
+
+if playing.lower() != "yes":
+  print("Come back when you want to play")
+  exit()
+
+
+print(playing)
