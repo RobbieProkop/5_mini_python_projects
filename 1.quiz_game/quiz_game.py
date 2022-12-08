32eb3963e01e210c9300f5e2ae9e0a557e90320d
Robbie Prokop <robbieprokop@gmail.com>
2022-12-08 05:18:47 +0400

    complete basic quiz game. Need to add more qs

--- 1.quiz_game/quiz_game.py
+++ 1.quiz_game/quiz_game.py
@@ -19,21 +19,41 @@ questionList = [
     'lecture': 'Mr. Armstrong said "This is one small step for a man, one giant leap for mankind" after stepping on the moon in July of 1969'
   },
   {
-    "question": "What is the national sport of Canada? ",
+    "question": "What is the national sport of Canada? ", 
     'answer' : 'lacrosse',
     'lecture': 'I bet you thought it was hockey!'
   },
   {
-    "question": "Who discovered gravity (First and last name)? ",
+    "question": "Who discovered gravity? ",
     'answer' : 'isaac newton',
     'lecture': 'Come on! Maybe you need to go back to elementary.'
+  },
+  {
+    "question": "What is the worlds longest river called? ",
+    'answer' : 'the nile',
+    'lecture': 'The Nile is in Egypt.'
+  },
+  {
+    "question": "Which country gifted the Statue of Liberty to the USA? ",
+    'answer' : 'france',
+    'lecture': 'France gave it as a congratulations for winning independance against Britain.'
+  },
+  {
+    "question": "How many bones are there in the human body? ",
+    'answer' : '206',
+    'lecture': 'That\' a lot of bones.'
   },
 ]
 
 def check_lives():
-  if lives != 0:
-    print(f'You have {lives} remaining')
-    return print("Next Question: ")
+  if (lives != 0) and (lives > 1):
+    print(f'You have {lives} lives remaining')
+    print("Next Question: ")
+    return print()
+  if lives == 1:
+    print(f'You have {lives} life remaining')
+    print("Next Question: ")
+    return print()
   
   print("You are out of lives. Game over!")
   exit()
@@ -50,24 +70,29 @@ def question():
   q = input(current[0])
   if q.lower() != current[1]:
     print(f'Sorry, the correct answer was {current[1]}.')
+    print()
     print(current[2])
+    print()
     global lives
     lives -= 1
     check_lives()
   else:
     print('Correct!')
     print("Next Question: ") 
+    print()
 
 
 print('Welcome to the quiz!')
-
 print()
 
 print("Rules: Please write all of your answers in lowecase, otherwise they will be considered incorrect")
-
 print()
 
-playing = input("Would you like to play? ")
+print("You must go through 10 questions, with no more than 3 wrong answers to win! If the question asks for the name of a person, please enter the fist and last name")
+print()
+
+playing = input("Are you ready to start? ")
+print()
 
 if playing.lower() != "yes":
   print("Come back when you want to play")
@@ -90,4 +115,4 @@ question()
 #  4th question
 question()
 
-print("Congratulation, you won the game!")
+print(f"Congratulation, you won the game with {lives} lives remaining!")
