034925a0cebdd99f62f9e3ae9e741284e1aca8e0
Robbie Prokop <robbieprokop@gmail.com>
2022-12-09 14:03:03 -0700

    implement basic number guesser game

--- 2.number_guesser/main.py
+++ 2.number_guesser/main.py
@@ -1,3 +1,28 @@
 import random
 
-print(random.randrange(-1, 10))
+
+a = 1
+b = 10
+
+r = random.randint(a, b)
+
+guess= ""
+
+def guess_again():
+  global guess
+  guess = input(f"Guess a number between {a} and {b}: ")
+  if int(guess) > r:
+    print(f"the number is less than {guess}")
+    guess_again()
+
+  if int(guess) < r:
+    print(f"the number is greater than {guess}")
+    guess_again()
+
+  if int(guess) == r:
+    return
+  
+
+# guess = input(f"Guess a number between {a} and {b}: ")
+guess_again()
+print('Congratulations! You win!')
