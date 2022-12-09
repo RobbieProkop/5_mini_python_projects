19f38691239c6d6e39e57d6ae93c872027680959
Robbie Prokop <robbieprokop@gmail.com>
2022-12-09 14:27:50 -0700

    guess game works with guess function

--- 2.number_guesser/main.py
+++ 2.number_guesser/main.py
@@ -2,27 +2,42 @@ import random
 
 
 a = 1
-b = 10
+b = 100
 
 r = random.randint(a, b)
+
+numOfGuess = 0
 
 guess= ""
 
 def guess_again():
   global guess
+  global numOfGuess
   guess = input(f"Guess a number between {a} and {b}: ")
-  if int(guess) > r:
-    print(f"the number is less than {guess}")
-    guess_again()
-
-  if int(guess) < r:
-    print(f"the number is greater than {guess}")
-    guess_again()
-
-  if int(guess) == r:
-    return
   
+  if not guess.isdigit():
+    print("Invalid entry")
+    guess_again()
+  guess = int(guess)
+  if guess <= a or guess >= b:
+    print(f"Please enter a number between {a} and {b}")
+    guess_again()
+  if guess > r:
+    print(f"the number is less than {guess}")
+    numOfGuess += 1
+    print("first ",numOfGuess)
+    guess_again()
 
-# guess = input(f"Guess a number between {a} and {b}: ")
+  if guess < r:
+    print(f"the number is greater than {guess}")
+    numOfGuess += 1
+    print("second ",numOfGuess)
+    guess_again()
+
+  if guess == r:
+    numOfGuess += 1
+    print(f'Congratulations! You guessed the number with {numOfGuess} guesses')
+    quit()
+  
 guess_again()
-print('Congratulations! You win!')
+
