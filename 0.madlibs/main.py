a06755312ac388511b50068f447fe44c93cdf915
Robbie Prokop <robbieprokop@gmail.com>
2022-12-17 20:11:05 -0700

    string concat madlib game

---
+++ 0.madlibs/main.py
@@ -0,0 +1,9 @@
+adj = input("Adjective: ")
+adj2 = input("Adjective: ")
+verb = input("Verb: ")
+
+
+madlib = f"The war between Russia and Ukraine is {adj}. I feel {adj2} when I see {verb}. This madlib game will continue forever. "
+
+print(madlib)
+
